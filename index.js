93b04d162d4a8991cfdd3c13b2b59c5cdd510541
adjms <an.jmirandas@gmail.com>
2022-03-09 17:21:32 -0300

    nuevas lineas de código en index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+//Initializations
 const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+
+//Starting Server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+    //console.log(path.join(__dirname, "public/css"));
+});
